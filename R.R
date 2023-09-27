c8073094f39a469513dd640e97849938f8555653
leghairben <ben31267@gmail.com>
2023-09-27 14:51:49 +0800

    R

---
+++ R.R
@@ -0,0 +1,42 @@
+#Basic Data Type ----
+c("John", "Mary")
+c(2, 3.1412)
+c(TRUE, TRUE, F, T)
+typeof(c("John", "Mary"))
+typeof(list("John", 178, TRUE))
+
+#Binding ----
+personName = c("John", "Mary")
+interestingNumber = c(2, 3.1412)
+covidPositive = c(TRUE, TRUE, F)
+
+# name call to access value
+personName
+interestingNumber
+covidPositive
+
+personName
+
+# Retrieve ONE element value ----
+#  by position / index
+personName[[1]]
+personName[[2]]
+
+list("John", 178, TRUE)
+listExample = list("John", 178, TRUE)
+
+listExample
+listExample[[3]]
+
+# Name your element values ----
+## name: John
+
+
+"name"="John"
+"age"=38
+"height"=178
+"is married"=F
+
+
+
+  
