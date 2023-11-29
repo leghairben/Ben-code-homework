216341e234861e141fa36262323c5a1ab1f5fbd4
leghairben <ben31267@gmail.com>
2023-11-29 14:23:20 +0800

    1129

---
+++ 1129/1129.R
@@ -0,0 +1,55 @@
+dataSet <- list(
+  list(
+    gender="male", height=178, age=32,
+    children=list(
+      list(gender="male", age=5),
+      list(gender="male", age=0))
+  ),
+  list(
+    gender="female", height=166, age=30,
+    children=list(
+      list(gender="female", age=3))
+  ),
+  list(
+    gender="male", height=170, age=42,
+    children=list(
+      list(gender="male", age=10),
+      list(gender="female", age=8))
+  ),
+  list(
+    gender="male", height=188, age=22,
+    children=list()
+  ),  
+  list(
+    gender="male", height=172, age=52,
+    children=list(
+      list(gender="female", age=25),
+      list(gender="female", age=23))
+  ),
+  list(
+    gender="female", height=160, age=42,
+    children=list(
+      list(gender="female", age=11))
+  )
+)
+
+# pattern
+howManyTimes=length(dataSet)
+for(.x in 1:howManyTimes)
+{
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
