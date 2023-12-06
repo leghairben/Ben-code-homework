9334cb4c7018a9a185f73d6e0049425c61f0c4b4
leghairben <ben31267@gmail.com>
2023-12-06 13:44:43 +0800

    1206

---
+++ 1206/1206-2.R
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Task by situation
+
+# oneComingStudentMajor == "Economics"
+print("Please go to room 201.")
+print(oneComingStudentMajor)
+# oneComingStudentMajor == "Sociology"
+print("Please go to room 301.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
